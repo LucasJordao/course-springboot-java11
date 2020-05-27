c47d674108bd03eaae8816b6c7851fab72b44d16
Lucas William <59783795+LucasJordao@users.noreply.github.com>
2020-05-27 15:10:54 -0300

    OrderItem, many-to-many association with extra attributes

---
+++ src/main/java/com/jpacourse/course/entities/OrderItem.java
@@ -0,0 +1,97 @@
+package com.jpacourse.course.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import com.jpacourse.course.entities.pk.OrderItemPK;
+
+@Entity
+@Table(name = "tb_order_item")
+public class OrderItem implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	//Attributes
+	@EmbeddedId
+	private OrderItemPK id;
+	
+	private Integer quantity;
+	private Double price;
+	
+	//Constructor and Overloads
+	public OrderItem() {
+		
+	}
+
+	public OrderItem(Order order, Product product, Integer quantity, Double price) {
+		super();
+		this.quantity = quantity;
+		this.price = price;
+		id.setOrder(order);
+		id.setProduct(product);
+	}
+
+	//Getters and Setters
+	public Order getOrder() {
+		return this.id.getOrder();
+	}
+	
+	public void setOrder(Order order) {
+		this.id.setOrder(order);
+	}
+	
+	public Product getProduct() {
+		return this.id.getProduct();
+	}
+	
+	public void setProduct(Product product) {
+		this.id.setProduct(product);
+	}
+	
+	public Integer getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(Integer quantity) {
+		this.quantity = quantity;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	//HashCode and Equals
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		OrderItem other = (OrderItem) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+	
+}
