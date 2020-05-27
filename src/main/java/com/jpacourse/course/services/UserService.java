9d048094ed9339bc09265143a5f572519ec0ad3a
Lucas William <59783795+LucasJordao@users.noreply.github.com>
2020-05-27 17:58:04 -0300

    User insert and delete

--- src/main/java/com/jpacourse/course/services/UserService.java
+++ src/main/java/com/jpacourse/course/services/UserService.java
@@ -23,5 +23,13 @@ public class UserService {
 		Optional<User> obj = repository.findById(id);
 		return obj.get();
 	}
+	
+	public User insert(User obj) {
+		return repository.save(obj);
+	}
+	
+	public void delete(Long id) {
+		repository.deleteById(id);
+	}
 
 }
